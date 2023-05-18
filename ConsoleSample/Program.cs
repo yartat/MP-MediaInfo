2c7c1102b116e5480590c73f55c06d3a547cc755
Michael <mejevin@gmail.com>
2023-05-18 15:13:24 +0200

    Remove console sample

--- ConsoleSample/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediaInfo;
-using Microsoft.Extensions.Logging;
-
-var logger = LoggerFactory.Create(a => { }).CreateLogger<Program>();
-var wrapper = new MediaInfoWrapper("videos/video_with_rotation.mp4", logger);
-
-Console.WriteLine(wrapper.VideoRotation);
